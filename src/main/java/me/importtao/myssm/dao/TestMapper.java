6ca44ab11fde86624f963def18ca34e4baf17188
importtao <import_tao@163.com>
2017-11-28 11:12:07 +0800

    整合redis 加入缓存

--- src/main/java/me/importtao/myssm/dao/TestMapper.java
+++ src/main/java/me/importtao/myssm/dao/TestMapper.java
@@ -1,18 +1,17 @@
 package me.importtao.myssm.dao;
 
 import me.importtao.myssm.model.Test;
-import org.apache.ibatis.annotations.Param;
 
 public interface TestMapper {
-    int deleteByPrimaryKey(@Param("id")Integer id);
+    int deleteByPrimaryKey(Integer id);
 
-    int insert(@Param("record")Test record);
+    int insert(Test record);
 
-    int insertSelective(@Param("record")Test record);
+    int insertSelective(Test record);
 
-    Test selectByPrimaryKey(@Param("id") Integer id);
+    Test selectByPrimaryKey(Integer id);
 
-    int updateByPrimaryKeySelective(@Param("record")Test record);
+    int updateByPrimaryKeySelective(Test record);
 
-    int updateByPrimaryKey(@Param("record")Test record);
+    int updateByPrimaryKey(Test record);
 }
